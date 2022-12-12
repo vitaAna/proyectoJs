c1b6552015c5c36825098f9200377981288cc1eb
Florencia Vita <vitaanalauraflorencia@gmail.com>
2022-12-12 20:24:52 -0300

    objetos,arrays y funciones y metodos de busqueda

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,110 @@
 const Marca = "MV INDUMENTARIA";
-let nombre = prompt("ingrese su nombre para registrarse");
-console.log("ingresó " + nombre);
 
-if (nombre != " ") {
-    alert("hola " + nombre + "se registró con éxito en " + Marca
-     +"ya puede comprar!")
+//formulario del cliente
+class Cliente {
+    constructor(nombre, apellido, telefono, mail) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.mail = mail;
+
+    }
+    toString() {
+        return this.nombre + " " + this.apellido;
+    }
+}
+let clienteUno = new Cliente(prompt("Ingrese Su Nombre").toLocaleUpperCase(), prompt("Ingrese Su Apellido").toLocaleUpperCase(), prompt("Ingrese Su telefono").toLocaleUpperCase(), prompt("Ingrese Su Mail"));
+console.log("los valores del cliente son", clienteUno)
+console.log("el cliente se llama", clienteUno.toString())
+alert("gracias y bienvenidos a" + Marca)
+
+//simulando un carrito de compras utilizando arrays,bucle y metodos de busqueda
+let productos = [{
+    nombre: "remera",
+    precio: 1200
+},
+{
+    nombre: "short",
+    precio: 2000
+},
+{
+    nombre: "jeans",
+    precio: 7000
+},
+{
+    nombre: "vestido",
+    precio: 1500
+},
+{
+    nombre: "campera",
+    precio: 5000
+},
+];
+let carrito = []
+
+let seleccion = prompt("hola desea comprar un producto? ingrese si o no")
+while (seleccion != "si" && seleccion != "no") {
+alert("por favor ingrese si o no")
+seleccion = prompt("hola desea comprar un producto? ingrese si o no")
+}
+
+if (seleccion == "si") {
+alert("a continuacion nuestra lista de productos")
+let todosLosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$")
+alert(todosLosProductos.join(" \n "))
+} else if (seleccion == "no") {
+alert("gracias visitar" + Marca + "hasta pronto")
 
 }
 
-function sumar(numeroUno, numeroDos, numeroTres) {
-    console.log("el resultado es" + (parseFloat(uno) + parseFloat(dos) + parseFloat(tres)))
-}
-let uno = prompt("ingrese el valor de la primer prenda");
-let dos = prompt("ingrese el valor de la segunda prenda");
-let tres = prompt("ingrese el valor de la tercer prenda");
-sumar(uno, dos, tres);
+while (seleccion != "no") {
+let producto = prompt("agrega un producto a tu carrito")
+let precio = 0;
 
-alert("el total es " + (parseFloat(uno) + parseFloat(dos) + parseFloat(tres)));
+if (producto == "remera" || producto == "short" || producto == "jeans" || producto == "vestido" || producto == "campera") {
+    switch (producto) {
+        case "remera":
+            precio = 1200;
+            break;
+        case "short":
+            precio = 2000;
+            break;
+        case "jeans":
+            precio = 7000;
+            break;
+        case "vestido":
+            precio = 1500;
+            break;
+        case "campera":
+            precio = 5000;
+            break;
+        default:
+            break;
+    }
+
+    let unidades = parseInt(prompt("cuantas unidades quiere llevar?"));
+    carrito.push({
+        producto,
+        unidades,
+        precio
+    })
+    console.log(carrito);
+} else {
+    alert("no tenemos ese producto");
+}
+seleccion = prompt("desea seguir comprando? si o no")
+while (seleccion == "no") {
+    alert("gracias por la compra, hasta pronto" + Marca)
+    carrito.forEach((carritoFinal) => {
+        console.log(`producto:${carritoFinal.producto}, unidades:${carritoFinal.unidades}, total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
+    })
+    break;
+}
+}
+
+let total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
+console.log(`el total a pagar por su compra es:${total}`);
+alert(`el total a pagar por su compra es:${total}`);
 
 
 let envios = prompt("ingrese su zona para saber el valor de envios :" + "\n" + "1) zona este" + "\n" + "2) zona este" + "\n" + "3) zona norte" + "\n" + "4) zona sur");
@@ -32,7 +120,9 @@ if (envios == "1" || envios.toLowerCase() == "zona oste") {
 } else if (envios == "4" || envios.toLowerCase() == "zona sur") {
     console.log("el valor es de $2000");
     alert("el valor es de $2000")
-}
+}else{
+    alert("debe ingresar un numer del 1 al 4")
+    }
 
 let zona = parseInt(prompt("elige tu zona para saber los dias de entrega:" + "\n" + "1-Zona Oeste" + "\n" + "2-Zona Este" + "\n" + "3-Zona Norte" + "\n" + "4-Zona Sur."));
 
@@ -54,8 +144,3 @@ switch (zona) {
         break;
 }
 
-let inicio = prompt("para conseguir un descuento solo tenes que elegir un numero del 1 al 10")
-while (inicio != "7") {
-    alert(inicio + "segui participando");
-    inicio = prompt("desea finalizar?ingrese 1, de lo contrario ingrese 2")
-}
